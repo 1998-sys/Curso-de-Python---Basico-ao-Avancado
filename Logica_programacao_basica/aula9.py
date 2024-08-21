af50be32e8b1712ec651856a184b6d89b4aec130
Matheus <matheus.bandeira74@gmail.com>
2024-08-21 07:11:57 -0300

    aula 09

---
+++ Logica_programacao_basica/aula9.py
@@ -0,0 +1,29 @@
+# Operações aritméticas
+
+adicao = 10 + 10
+print('Adiçao', adicao)
+
+subtracao = 10 - 5
+print('Subtracao', subtracao)
+
+multiplicacao = 10 * 10
+print('Multiplicação', multiplicacao)
+
+divisao = 10 / 2.2
+print('Divisao', divisao)
+
+divisao_inteira = 10 // 2.2
+print('Divisao Inteira', divisao_inteira)
+
+exponenciacao = 2 ** 10
+print('Exponenciação', exponenciacao)
+
+modulo = 55 % 2 #resto da divisão
+print('Módulo', modulo)
+
+
+# Ex: multiplo de 8
+print(10 % 8 == 0)
+print(16 % 8 == 0)
+print(10 % 2 == 0)
+print(15 % 2 == 0)
