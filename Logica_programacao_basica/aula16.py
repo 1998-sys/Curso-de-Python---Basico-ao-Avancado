e299a2476fefdb5450b4f20cd7ff124c0d805d78
Matheus <matheus.bandeira74@gmail.com>
2024-08-21 07:56:15 -0300

    input

---
+++ Logica_programacao_basica/aula16.py
@@ -0,0 +1,12 @@
+# Input (pegar informações do usuário)
+
+input('Qual o seu nome? ')
+
+# armazenando a informação input em uma variável - Input por padrão sempre vai retornar uma STRING
+nome = input('Qual o seu nome? ')
+print(f'O seu nome é: {nome=}') # imprime o nome da variável e valor 
+
+numero1 = input('Digite um número: ')
+numero2 = input('Digite outro número: ')
+
+print(f'A soma dos números é: {numero1 + numero2}') # Concatenação pois sempre trará uma string 
