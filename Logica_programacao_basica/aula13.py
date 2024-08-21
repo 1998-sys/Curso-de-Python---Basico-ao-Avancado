f2cd620be3eb5133fc68bf2a42fec3d99e6ba36d
Matheus <matheus.bandeira74@gmail.com>
2024-08-21 07:23:04 -0300

    IMC

---
+++ Logica_programacao_basica/aula13.py
@@ -0,0 +1,14 @@
+# IMC - Exercício
+
+nome = 'Matheus Bandeira'
+altura = 1.83
+peso = 95
+imc = peso / (altura ** 2)
+
+print(nome, 'tem', altura, 'de altura\npesa', peso, 'quilos e seu IMC é ', imc
+      )
+
+# place holder - elipses (vai escrever um código mais ainda não desenvolveu)
+
+teste  = ...
+print(teste) # Elipsis
