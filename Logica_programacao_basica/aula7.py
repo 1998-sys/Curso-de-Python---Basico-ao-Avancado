b3f7de8df2950e086bf69be1270bae139cdb18fb
Matheus <matheus.bandeira74@gmail.com>
2024-08-20 21:47:23 -0300

    Atribuicao de variaveis

---
+++ Logica_programacao_basica/aula7.py
@@ -0,0 +1,19 @@
+# Variáveis são usadas para salvar algo na memória do computador.
+# PEP8: inicie as variáveis com letras minúsculas, pode usar
+# Números e underline _.
+# O sinal de = é o operador de atribuição. ELe é usado para 
+# Atribuir um valor a um nome (Variável)
+# USo: nome_variavel = expressão
+
+# nome_completo = 'Matheus Augusto do Nascimento Bandeira'
+# soma_dois_mais_dois = 2+2
+# int_um = bool('1')
+# print(nome_completo,soma_dois_mais_dois)
+# print(int('1'), type(int_um))
+
+
+nome = 'Matheus'
+idade = 26
+maior_de_idade = idade >= 18
+print(maior_de_idade)
+print('Nome: ', nome, 'Idade', idade)
