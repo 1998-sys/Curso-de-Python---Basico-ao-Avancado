09e1120b27408481eca8725ff64d8c59e8fe663f
Matheus <matheus.bandeira74@gmail.com>
2024-08-22 21:59:34 -0300

    if else e elif

--- Logica_programacao_basica/aula17.py
+++ Logica_programacao_basica/aula17.py
@@ -15,9 +15,16 @@
 
 # condicao
 condicao = False
+condicao1 = False
+condicao2 = True
+
 
 if condicao:
     print('Este é o código do if')
+elif condicao1:
+    print('Este é o código da primeira condição ')
+elif condicao2:
+    print('Este é o código da segunda condição')
 else:
     print('Este é o else do primeiro if')
 
@@ -25,3 +32,4 @@ print('fora do if')
 
 if  10 == 10:
     print('outro if')
+
