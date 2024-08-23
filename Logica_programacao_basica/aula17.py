6c84250a353feef6b7d603e5a285411c2f639403
Matheus <matheus.bandeira74@gmail.com>
2024-08-22 21:54:24 -0300

    revisão if else

--- Logica_programacao_basica/aula17.py
+++ Logica_programacao_basica/aula17.py
@@ -1,13 +1,27 @@
 # if / elif         /else
 # se/ se não se     / se não
 
-entrada = input('Você quer "entrar" ou "sair" ? ')
+# entrada = input('Você quer "entrar" ou "sair" ? ')
 
-if entrada == 'entrar':
-    print('Você entrou no sistema')
-elif entrada == 'sair':
-    print('Você saiu do sistema')
+# if entrada == 'entrar':
+#     print('Você entrou no sistema')
+# elif entrada == 'sair':
+#     print('Você saiu do sistema')
+# else:
+#     print('Você não digitou nem entrar nem sair')
+
+# print('Fora dos blocos')
+
+
+# condicao
+condicao = False
+
+if condicao:
+    print('Este é o código do if')
 else:
-    print('Você não digitou nem entrar nem sair')
+    print('Este é o else do primeiro if')
 
-print('Fora dos blocos')
+print('fora do if')
+
+if  10 == 10:
+    print('outro if')
