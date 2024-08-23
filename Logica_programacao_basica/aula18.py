ce9716cba67fc6964eda85be449fe7408d01a603
Matheus <matheus.bandeira74@gmail.com>
2024-08-22 22:13:48 -0300

    debugger

---
+++ Logica_programacao_basica/aula18.py
@@ -0,0 +1,20 @@
+#Debugger
+# condicao
+condicao = False
+condicao1 = False
+condicao2 = True
+
+
+if condicao:
+    print('Este é o código do if')
+elif condicao1:
+    print('Este é o código da primeira condição ')
+elif condicao2:
+    print('Este é o código da segunda condição')
+else:
+    print('Este é o else do primeiro if')
+
+print('fora do if')
+
+if  10 == 10:
+    print('outro if')
