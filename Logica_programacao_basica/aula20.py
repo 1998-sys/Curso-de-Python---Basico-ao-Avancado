51073e30fdfc82707e089375d82091a181b69720
Matheus <matheus.bandeira74@gmail.com>
2024-08-22 22:30:45 -0300

    exercicio if else

---
+++ Logica_programacao_basica/aula20.py
@@ -0,0 +1,9 @@
+# Exercício
+
+primeiro_valor = input('Digite um valor: ')
+segundo_valor = input('Digite outro valor')
+
+if primeiro_valor > segundo_valor:
+    print(f'primeiro valor {primeiro_valor=} é o maior')
+else:
+    print(f'Segundo valor {segundo_valor=} é o maior')
