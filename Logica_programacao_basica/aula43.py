e42b7bb4b9052290ebfa36109385a44b7a1e9208
Matheus <matheus.bandeira74@gmail.com>
2024-08-25 20:59:40 -0300

    for com range

---
+++ Logica_programacao_basica/aula43.py
@@ -0,0 +1,17 @@
+"""
+For + Range
+range -> range(start, stop, step)
+
+O último valor não é incluído
+"""
+
+# numeros = range(10)
+# print(numeros[5])
+
+# for numeros in range(5, -10, -1):
+#     print(numeros)
+
+# pegando os números pares
+for numeros in range(0,100,2):
+    print(numeros)
+
