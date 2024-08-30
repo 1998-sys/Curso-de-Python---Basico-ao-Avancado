d7ecc536f4f39a09d87efb26d29217f41888594a
Matheus <matheus.bandeira74@gmail.com>
2024-08-29 21:54:44 -0300

    Tuplas

---
+++ Logica_programacao_basica/aula54.py
@@ -0,0 +1,12 @@
+"""
+Tipo Tupla - uma lista imutável
+"""
+nomes = "Maria", 'Helena', 'Luiz'
+print(nomes)
+print(type(nomes))
+
+##### Convertendo uma lista em tupla
+
+lista = ['João',123]
+tupla = tuple(lista)
+print(tupla)
