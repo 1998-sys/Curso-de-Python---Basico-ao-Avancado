9d788de6bad19b74f4c56dc62e42cf2aa0587212
Matheus <matheus.bandeira74@gmail.com>
2025-01-12 20:54:43 -0300

    format

--- Logica_programacao_basica/aula15.py
+++ Logica_programacao_basica/aula15.py
@@ -4,7 +4,11 @@ a = 'AAAA'
 b = 'BBBBB'
 c = 1.1
 # Na ordem vem o primeiro argumento da função format
-string = 'a={0} a= {0}b={1} c{2:.2f}'
-formato = string.format(a,b,c)
+# string = f'a={0} a= {0}b={1} c{2:.2f}'  #2.f formata as casa decimais
+# formato = string.format(a,b,c)
 
+
+
+# Formatação utilzando o comando format
+formato = 'a={0} b={1} c={2:.3f}'.format(a,b,c) # As chaves refereciam os valores também é possível utilizar índices
 print(formato)
